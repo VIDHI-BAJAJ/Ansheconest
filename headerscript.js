874a395f218306c9d1544c0f5a428fb44e0cb19f
Vidhi Bajaj <vidhibajaj1401@gmail.com>
2025-03-12 12:45:28 +0530

    second

--- headerscript.js
+++ headerscript.js
@@ -60,3 +60,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("fixed");
+    } else {
+        navbar.classList.remove("fixed");
+    }
+});
